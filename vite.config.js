76c6d399c2434e5a5160c167be193185b1937feb
Suveer <suveer@Adhyots-MacBook-Air.local>
2026-04-02 18:19:41 +0530

    fix: move index.html to src and update vite config for render

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: true,
+  }
 })
