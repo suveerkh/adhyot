0ada8f97abb978abe13c53e646e61aa77d5955b5
Suveer <suveer@Adhyots-MacBook-Air.local>
2026-04-04 22:45:41 +0530

    renamed keys to use _VITE as prefix in .env and supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_PUBLISHABLE
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE
 )
 
 export default supabase
