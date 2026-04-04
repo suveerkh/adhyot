6ec64c96fc908a7813075acaaac8301037fe74ad
Suveer <suveer@Adhyots-MacBook-Air.local>
2026-04-04 22:38:21 +0530

    Removed index.html check in src from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   build: {
-    outDir: '../server/dist',
+    outDir: 'dist',
     emptyOutDir: true,
   }
 })
